23b8a34921b274d678da5b131ae3c095c3c60c55
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2025-02-10 18:16:52 -0600

    Added a new practice

---
+++ chapter_2/ticket.rb
@@ -0,0 +1,45 @@
+ticket = Object.new
+
+def ticket.date
+  "1903-01-02"
+end
+
+def ticket.venue
+  "Town Hall"
+end
+
+def ticket.event
+  "Author's reading"
+end
+
+def ticket.performer
+  "Mark Twain"
+end
+
+def ticket.seat
+  "Second Balcony, row J, seat 12"
+end
+
+def ticket.price
+  5.50
+end
+
+def ticket.available?
+  false
+end
+
+print "Information desired: "
+request = gets.chomp
+
+if ticket.respond_to?(request)
+  puts ticket.send(request)
+else
+  puts "No such information available"
+end
+
+puts "Additional Information below: "
+
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}.\n" +
+"The performer is #{ticket.performer}.\n" +
+"The seat is #{ticket.seat}, " +
+"and it costs $#{"%.2f." % ticket.price}"
